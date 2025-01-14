ca63173f7a9e1b8fce99804eb797c8f37f0e4f8e
treybowman68 <kenttrey68@gmail.com>
2025-01-14 13:33:27 -0700

    test

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
-    }
+        var x= 8;
+        var y= 4;
+        
+        
+        if (x > y)
+{
+    Console.WriteLine("greater than");
 }
+    }
+
+
+}
+ 
